9d08de1547bbf132f62de98c7a9c2696977352b2
RayaFarshad <rayafarshad@gmail.com>
2020-11-16 14:36:01 +0330

    Update generalRoutes.js

--- application/src/routes/generalRoutes.js
+++ application/src/routes/generalRoutes.js
@@ -19,19 +19,10 @@ router.get("/fail",(req, res) => {
 
 });
 
-
-// router.get("/history",(req, res) => {
-
-//     res.render("history", {
-
-
-//     });
-// });
-
 router.get("/generateLink",(req, res) => {
     console.log(req.query);
     let classID = req.query.classID;
-    let pasClassID = "http://localhost:3000/EmojiSharing/?classID="+classID;
+    let pasClassID = "http://13.57.196.89/:3000/EmojiSharing/?classID="+classID;
 
     res.render("generateLink", {
         classID: pasClassID
