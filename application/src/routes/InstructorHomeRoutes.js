5c580c499aaf302cd7af344148b93fd0b9df61d6
RayaFarshad <rayafarshad@gmail.com>
2020-11-24 19:43:41 +0330

    try catch

--- application/src/routes/InstructorHomeRoutes.js
+++ application/src/routes/InstructorHomeRoutes.js
@@ -20,48 +20,71 @@ async function insertInstructure(req, res, next) {
     console.log(hash);
     console.log(req.body.name);
     let query = " INSERT INTO emoji_db.users (full_name, email, password, isInstructor) VALUES ( '" +req.body.name+ "' , '"+ req.body.email +"' , '"+ hash +"', 1)";
-    console.log(query);
-    await db.execute(query, (err, res) => {
-        console.log(query); 
-        
-        // you can show alert if the user is already exists
-        if (err) throw err;
-        next();
-    });
+    // console.log(query);
+    try{
+        await db.execute(query, (err, res) => {
+            console.log(query); 
+            
+            // you can show alert if the user is already exists
+            if (err) throw err;
+            next();
+        });
+    }
+    catch(e) {
+        console.log('Catch an error: ', e)
+      }
 }
 
 async function getInstructorID(req, res, next) {
 
     let query = " SELECT * FROM emoji_db.users where email = '"+req.body.email+"'";
     console.log("hellloooo2"); 
-    await db.execute(query, (err, res) => {
-        if (err) throw err;
-        // console.log(query); 
-        req.instructorID = res[0].id;
-        // console.log(req.instructorID);    
-        next();
-    });
+    try {
+        await db.execute(query, (err, res) => {
+            if (err) throw err;
+            console.log(query); 
+            req.instructorID = res[0].id;
+            // console.log(req.instructorID);    
+            next();
+        });
+      }
+      catch(e) {
+        console.log('Catch an error: ', e)
+      }
+
+    
 }
 
 async function insertClasses(req, res, next) {
     // let query = " SELECT * FROM emoji_db.instructors where email = '"+req.body.email+"'";
     let query = " INSERT INTO emoji_db.classes (id, class_name, datetime, startTime, endTime ) VALUES ( " +req.instructorID+ " ,'" +req.body.className+ "' , '"+ req.body.weekday+ "-" + req.body.startTime + ","+req.body.endTime+ "' , '"+ req.body.startTime  +"', '"+ req.body.endTime+"' )";
-    await db.execute(query, (err, res) => {
-        console.log(query);
-        if (err) throw err;
-        next();
-    });
+    try{
+        await db.execute(query, (err, res) => {
+            console.log(query);
+            if (err) throw err;
+            next();
+        });
+    }
+    catch(e) {
+        console.log('Catch an error: ', e)
+      }
+
 }
 async function getClassID(req, res, next) {
     let query = " SELECT * FROM emoji_db.classes where datetime = '"+req.body.weekday+ "-" + req.body.startTime + ","+req.body.endTime+"'";
-    await db.execute(query, (err, res) => {
-        // console.log(query);
-        req.classID = res[0].id;
-        // console.log(req.classID);
-        // you can show alert if the user is already exists
-        if (err) throw err;
-        next();
-    });
+    try{
+        await db.execute(query, (err, res) => {
+            console.log(query);
+            req.classID = res[0].id;
+            // console.log(req.classID);
+            // you can show alert if the user is already exists
+            if (err) throw err;
+            next();
+        });
+    }
+    catch(e) {
+        console.log('Catch an error: ', e)
+      }
 }
 
 
@@ -76,12 +99,17 @@ router.get("/home",(req, res) => {
 
 async function insertToRegisteration(req, res, next) {
     let query = " INSERT INTO emoji_db.registerations (classes_id, users_id, isInstructor) VALUES ( " +req.classID+ " ," +req.instructorID+ " , 1 )";
-  
+  try{
     await db.execute(query, (err, res) => {
-        // console.log(query);
+        console.log(query);
         if (err) throw err;
         next();
     });
+  }
+  catch(e) {
+    console.log('Catch an error: ', e)
+  }
+    
 }
 
 router.post("/home", insertInstructure, getInstructorID, insertClasses, getClassID, insertToRegisteration, (req, res) => {
