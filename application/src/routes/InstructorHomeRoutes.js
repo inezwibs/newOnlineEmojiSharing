bc41899c4a085aae7d2a2c82b1dd4c888b661925
RayaFarshad <rayafarshad@gmail.com>
2020-11-26 16:06:36 +0330

    Update InstructorHomeRoutes.js

--- application/src/routes/InstructorHomeRoutes.js
+++ application/src/routes/InstructorHomeRoutes.js
@@ -63,7 +63,9 @@ async function getInstructorID(req, res, next) {
         res = await db.execute(query);
         console.log(query); 
         test = res;
+        console.log(test);
         console.log("test: "+test); 
+        console.log(test[0]);
         console.log("test[0]: "+test[0]); 
         console.log("test.id: "+test.id); 
         next();
